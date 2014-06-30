e14809f084ef5405ed2459a358af8f4c347c45ba
KentHuang <kh395@cornell.edu>
2014-06-30 17:49:11 -0400

    finished

--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -13,7 +13,7 @@
 @property float register1;
 @property float register2;
 @property float register3;
-@property (nonatomic)  float tens;
+@property (nonatomic) float tens;
 @property (strong, nonatomic) NSString *operation;
 @property BOOL dot;
 @end
@@ -22,73 +22,88 @@
 
 - (float)tens {
     if (!_tens) {
-        _tens = 0.1;
+        _tens = 1;
     }
     return _tens;
 }
 
 - (IBAction)touchDigitButton:(UIButton *)sender {
+    
+    if ([_operation isEqualToString:@"="]) {
+        [self reset];
+    }
+    
     if (!_operation) {
         _register1 *= _dot ? 1.0 : 10.0;
         _register1 += _dot ? 0.0 : sender.currentTitle.intValue;
         _register3 *= _dot ? 10.0 : 1.0;
         _register3 += _dot ? sender.currentTitle.intValue : 0.0;
-        _register1 = _register1 + _tens * _register3;
-        _tens = 0.1 * _tens;
-        NSString *one = [NSString stringWithFormat:@"%g", _register1];
-        NSString *three = [NSString stringWithFormat:@"%g", _register3];
-        _display.text = _dot ? [NSString stringWithFormat:@"%@%@%@", one, @".", three] : one;
+        self.tens *= (_dot ? 0.1 : 1);
+        _display.text = [NSString stringWithFormat:@"%g", _register1 + self.tens * _register3];
     } else {
         _register2 *= _dot ? 1.0 : 10.0;
         _register2 += _dot ? 0.0 : sender.currentTitle.intValue;
         _register3 *= _dot ? 10.0 : 1.0;
         _register3 += _dot ? sender.currentTitle.intValue : 0.0;
-        _register2 = _register2 + _tens * _register3;
-        _tens = 0.1 * _tens;
-        NSString *two = [NSString stringWithFormat:@"%g", _register2];
-        NSString *three = [NSString stringWithFormat:@"%g", _register3];
-        _display.text = _dot ? [NSString stringWithFormat:@"%@%@%@", two, @".", three] : two;
+        self.tens *= (_dot ? 0.1 : 1);
+        _display.text = [NSString stringWithFormat:@"%g", _register2 + self.tens * _register3];
     }
 }
 
 - (IBAction)touchOperationButton:(UIButton *)sender {
     if (_register1) {
-        [self eval];
+        if (_operation) {
+            _register2 = _register2 + self.tens * _register3;
+            [self eval];
+            _register2 = 0;
+        } else {
+            _register1 = _register1 + self.tens * _register3;
+        }
+        
         _operation = sender.currentTitle;
-        _tens = 0.1;
-        _dot = NO;
-        _register3 = 0;
     }
+    
+    _dot = NO;
+    _register3 = 0;
+    _tens = 1;
 }
 
 - (void)eval {
+    
+//    NSLog(@"%f", _register1);
+//    NSLog(@"%f", _register2);
+//    NSLog(@"%f", _register3);
+//    NSLog(@"%f", _tens);
+    
     if ([_operation isEqualToString:@"/"]) {
         _register1 = _register1 / _register2;
-        _display.text = [NSString stringWithFormat:@"%f", _register1];
+        _display.text = [NSString stringWithFormat:@"%g", _register1];
     } else if ([_operation isEqualToString:@"*"]) {
         _register1 = _register1 * _register2;
-        _display.text = [NSString stringWithFormat:@"%f", _register1];
+        _display.text = [NSString stringWithFormat:@"%g", _register1];
     } else if ([_operation isEqualToString:@"-"]) {
         _register1 = _register1 - _register2;
-        _display.text = [NSString stringWithFormat:@"%f", _register1];
+        _display.text = [NSString stringWithFormat:@"%g", _register1];
     } else if ([_operation isEqualToString:@"+"]) {
         _register1 = _register1 + _register2;
-        _display.text = [NSString stringWithFormat:@"%f", _register1];
+        _display.text = [NSString stringWithFormat:@"%g", _register1];
     } else {
         
     }
-    
-    _register2 = 0;
-    _operation = nil;
+    NSLog(@"%f", _register1);
 }
 
 - (IBAction)touchClearButton:(UIButton *)sender {
     _display.text = @"0";
+    [self reset];
+}
+
+- (void)reset {
     _register1 = 0;
     _register2 = 0;
     _register3 = 0;
     _dot = NO;
-    _tens = 0.1;
+    _tens = 1;
     _operation = nil;
 }
 
