facd7571177e168286cac7be990cb024fe0f7072
KentHuang <kh395@cornell.edu>
2014-06-30 14:57:54 -0400

    dot not working

--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -12,44 +12,68 @@
 @property (weak, nonatomic) IBOutlet UILabel *display;
 @property float register1;
 @property float register2;
+@property float register3;
+@property (nonatomic)  float tens;
 @property (strong, nonatomic) NSString *operation;
+@property BOOL dot;
 @end
 
 @implementation CalculatorViewController
 
+- (float)tens {
+    if (!_tens) {
+        _tens = 0.1;
+    }
+    return _tens;
+}
+
 - (IBAction)touchDigitButton:(UIButton *)sender {
     if (!_operation) {
-        _register1 *= 10.0;
-        _register1 += sender.currentTitle.intValue;
-        _display.text = [NSString stringWithFormat:@"%g", _register1];
+        _register1 *= _dot ? 1.0 : 10.0;
+        _register1 += _dot ? 0.0 : sender.currentTitle.intValue;
+        _register3 *= _dot ? 10.0 : 1.0;
+        _register3 += _dot ? sender.currentTitle.intValue : 0.0;
+        _register1 = _register1 + _tens * _register3;
+        _tens = 0.1 * _tens;
+        NSString *one = [NSString stringWithFormat:@"%g", _register1];
+        NSString *three = [NSString stringWithFormat:@"%g", _register3];
+        _display.text = _dot ? [NSString stringWithFormat:@"%@%@%@", one, @".", three] : one;
     } else {
-        _register2 *= 10.0;
-        _register2 += sender.currentTitle.intValue;
-        _display.text = [NSString stringWithFormat:@"%g", _register2];
+        _register2 *= _dot ? 1.0 : 10.0;
+        _register2 += _dot ? 0.0 : sender.currentTitle.intValue;
+        _register3 *= _dot ? 10.0 : 1.0;
+        _register3 += _dot ? sender.currentTitle.intValue : 0.0;
+        _register2 = _register2 + _tens * _register3;
+        _tens = 0.1 * _tens;
+        NSString *two = [NSString stringWithFormat:@"%g", _register2];
+        NSString *three = [NSString stringWithFormat:@"%g", _register3];
+        _display.text = _dot ? [NSString stringWithFormat:@"%@%@%@", two, @".", three] : two;
     }
-    
 }
 
 - (IBAction)touchOperationButton:(UIButton *)sender {
     if (_register1) {
         [self eval];
         _operation = sender.currentTitle;
+        _tens = 0.1;
+        _dot = NO;
+        _register3 = 0;
     }
 }
 
 - (void)eval {
     if ([_operation isEqualToString:@"/"]) {
         _register1 = _register1 / _register2;
-        _display.text = [NSString stringWithFormat:@"%g", _register1];
+        _display.text = [NSString stringWithFormat:@"%f", _register1];
     } else if ([_operation isEqualToString:@"*"]) {
         _register1 = _register1 * _register2;
-        _display.text = [NSString stringWithFormat:@"%g", _register1];
+        _display.text = [NSString stringWithFormat:@"%f", _register1];
     } else if ([_operation isEqualToString:@"-"]) {
         _register1 = _register1 - _register2;
-        _display.text = [NSString stringWithFormat:@"%g", _register1];
+        _display.text = [NSString stringWithFormat:@"%f", _register1];
     } else if ([_operation isEqualToString:@"+"]) {
         _register1 = _register1 + _register2;
-        _display.text = [NSString stringWithFormat:@"%g", _register1];
+        _display.text = [NSString stringWithFormat:@"%f", _register1];
     } else {
         
     }
@@ -62,10 +86,15 @@
     _display.text = @"0";
     _register1 = 0;
     _register2 = 0;
+    _register3 = 0;
+    _dot = NO;
+    _tens = 0.1;
     _operation = nil;
 }
 
 - (IBAction)touchDotButton:(UIButton *)sender {
+    _dot = YES;
+    _display.text = [_display.text stringByAppendingString:@"."];
 }
 
 @end
